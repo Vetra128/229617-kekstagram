f4cf004474e663dd8dec215d8e3fff3d4cb53291
Elvina <elvina.ri@bk.ru>
2018-06-18 08:53:06 +0400

    fix names and replace function

--- js/pictures.js
+++ js/pictures.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var NUMBER_OF_PHOTOS = 25;
+var PHOTOS = 25;
 var MIN_LIKES = 15;
 var MAX_LIKES = 200;
-var MIN_NUMBER_OF_COMMEMT = 1;
-var MAX_NUMBER_OF_COMMEMT = 2;
-var ARRAY_OF_COMMENTS = [
+var MIN_COUNT_COMMENT = 1;
+var MAX_COUNT_COMMENT = 2;
+var COMMENTS = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -13,7 +13,7 @@ var ARRAY_OF_COMMENTS = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
-var ARRAY_OF_DESCRIPTIONS = [
+var DESCRIPTIONS = [
   'Тестим новую камеру!',
   'Затусили с друзьями на море',
   'Как же круто тут кормят',
@@ -23,73 +23,70 @@ var ARRAY_OF_DESCRIPTIONS = [
 ];
 var MIN_INDEX_AVATAR = 1;
 var MAX_INDEX_AVATAR = 6;
+var MAX_INDEX_COMMENT = COMMENTS.length - 1;
+var MAX_INDEX_DESCRIPTION = DESCRIPTIONS.length - 1;
 
 var randomInteger = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-var LengthArrayOfComments = ARRAY_OF_COMMENTS.length;
-var LengthArrayOfDescriptions = ARRAY_OF_DESCRIPTIONS.length;
-
 var fillPhotoComments = function (photo) {
-  if (randomInteger(MIN_NUMBER_OF_COMMEMT, MAX_NUMBER_OF_COMMEMT) === 2) {
-    var firstComment = ARRAY_OF_COMMENTS[randomInteger(0, LengthArrayOfComments - 1)];
-    var secondComment = ARRAY_OF_COMMENTS[randomInteger(0, LengthArrayOfComments - 1)];
+  if (randomInteger(MIN_COUNT_COMMENT, MAX_COUNT_COMMENT) === MAX_COUNT_COMMENT) {
+    var firstComment = COMMENTS[randomInteger(0, MAX_INDEX_COMMENT)];
+    var secondComment = COMMENTS[randomInteger(0, MAX_INDEX_COMMENT)];
     while (firstComment === secondComment) {
-      secondComment = ARRAY_OF_COMMENTS[randomInteger(0, LengthArrayOfComments - 1)];
+      secondComment = COMMENTS[randomInteger(0, MAX_INDEX_COMMENT)];
     }
     photo.comments = [firstComment, secondComment];
   } else {
-    photo.comments = [ARRAY_OF_COMMENTS[randomInteger(0, LengthArrayOfComments - 1)]];
+    photo.comments = [COMMENTS[randomInteger(0, MAX_INDEX_COMMENT)]];
   }
 };
 
 var createArrayOfPhotos = function () {
   var photos = [];
-  for (var i = 1; i <= NUMBER_OF_PHOTOS; i++) {
+  for (var i = 1; i <= PHOTOS; i++) {
     var photo = {};
     photo.url = 'photos/' + i + '.jpg';
     photo.likes = randomInteger(MIN_LIKES, MAX_LIKES);
     fillPhotoComments(photo);
-    photo.description = ARRAY_OF_DESCRIPTIONS[randomInteger(0, LengthArrayOfDescriptions - 1)];
+    photo.description = DESCRIPTIONS[randomInteger(0, MAX_INDEX_DESCRIPTION)];
     photos.push(photo);
   }
   return photos;
 };
 
-var similarPhotoTemplate = document.querySelector('#picture').content;
-
 var createPhoto = function (photo) {
-  var photoElement = similarPhotoTemplate.cloneNode(true);
+  var similarPhotoTemplate = document.querySelector('#picture').content;
+  var photography = similarPhotoTemplate.cloneNode(true);
 
-  photoElement.querySelector('.picture__img').src = photo.url;
-  photoElement.querySelector('.picture__stat--likes').textContent = photo.likes;
-  photoElement.querySelector('.picture__stat--comments').textContent = photo.comments.length;
+  photography.querySelector('.picture__img').src = photo.url;
+  photography.querySelector('.picture__stat--likes').textContent = photo.likes;
+  photography.querySelector('.picture__stat--comments').textContent = photo.comments.length;
 
-  return photoElement;
+  return photography;
 };
 
-var detailedPhoto = document.querySelector('.big-picture');
-
-var showDetailedPhoto = function (detailedPhotoElement, photo) {
-  detailedPhotoElement.classList.remove('hidden');
+var showDetailedPhoto = function (detailedPhoto, photo) {
+  detailedPhoto.classList.remove('hidden');
 
   detailedPhoto.querySelector('.big-picture__img').src = photo.url;
   detailedPhoto.querySelector('.likes-count').textContent = photo.likes;
   detailedPhoto.querySelector('.comments-count').textContent = photo.comments.length;
 
-  var detailedPhotoCommentElement = detailedPhoto.querySelector('.social__comment').cloneNode(true);
+  var detailedPhotoComment = detailedPhoto.querySelector('.social__comment').cloneNode(true);
   var detailedPhotoCommentList = detailedPhoto.querySelector('.social__comments');
   var detailedPhotoPicture = detailedPhoto.querySelector('.social__picture');
   var detailedPhotoText = detailedPhoto.querySelector('.social__text');
   var detailedPhotoDescription = detailedPhoto.querySelector('.social__caption');
+  var fragment = document.createDocumentFragment();
 
   detailedPhotoDescription.textContent = photo.description;
 
-  for (i = 0; i < photo.comments.length; i++) {
+  for (var i = 0; i < photo.comments.length; i++) {
     detailedPhotoPicture.src = 'img/avatar-' + (randomInteger(MIN_INDEX_AVATAR, MAX_INDEX_AVATAR)) + '.svg';
     detailedPhotoText.textContent = photo.comments[i];
-    fragment.appendChild(detailedPhotoCommentElement);
+    fragment.appendChild(detailedPhotoComment);
   }
 
   detailedPhotoCommentList.appendChild(fragment);
@@ -100,17 +97,21 @@ var showDetailedPhoto = function (detailedPhotoElement, photo) {
   detailedPhotoLoadmore.classList.add('visually-hidden');
 };
 
+var createPhotoList = function () {
+  var similarPhotoList = document.querySelector('.pictures');
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < PHOTOS; i++) {
+
+    fragment.appendChild(createPhoto(photos[i]));
+  }
+  similarPhotoList.appendChild(fragment);
+};
+
+var detailedPhoto = document.querySelector('.big-picture');
+
 var photos = createArrayOfPhotos();
 
-var similarPhotoListElement = document.querySelector('.pictures');
-
-var fragment = document.createDocumentFragment();
-
-for (var i = 0; i < NUMBER_OF_PHOTOS; i++) {
-
-  fragment.appendChild(createPhoto(photos[i]));
-}
-
-similarPhotoListElement.appendChild(fragment);
+createPhotoList();
 
 showDetailedPhoto(detailedPhoto, photos[0]);
