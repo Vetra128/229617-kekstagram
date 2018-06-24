6e823bcc6d1340d54707ef5e89b0ff903c769e94
Elvina <elvina.ri@bk.ru>
2018-06-24 15:28:35 +0400

    rename some constants

--- js/pictures.js
+++ js/pictures.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var PHOTOS = 25;
-var MIN_LIKES = 15;
-var MAX_LIKES = 200;
-var MIN_COUNT_COMMENT = 1;
-var MAX_COUNT_COMMENT = 2;
+var PHOTOS_COUNT = 25;
+var LIKES_MIN = 15;
+var LIKES_MAX = 200;
+var COMMENTS_MIN_COUNT = 1;
+var COMMENTS_MAX_COUNT = 2;
 var COMMENTS = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -21,36 +21,36 @@ var DESCRIPTIONS = [
   'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
   'Вот это тачка!'
 ];
-var MIN_INDEX_AVATAR = 1;
-var MAX_INDEX_AVATAR = 6;
-var MAX_INDEX_COMMENT = COMMENTS.length - 1;
-var MAX_INDEX_DESCRIPTION = DESCRIPTIONS.length - 1;
+var INDEX_AVATAR_MIN = 1;
+var INDEX_AVATAR_MAX = 6;
+var INDEX_COMMENT_MAX = COMMENTS.length - 1;
+var INDEX_DESCRIPTION_MAX = DESCRIPTIONS.length - 1;
 
 var randomInteger = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 var fillPhotoComments = function (photo) {
-  if (randomInteger(MIN_COUNT_COMMENT, MAX_COUNT_COMMENT) === MAX_COUNT_COMMENT) {
-    var firstComment = COMMENTS[randomInteger(0, MAX_INDEX_COMMENT)];
-    var secondComment = COMMENTS[randomInteger(0, MAX_INDEX_COMMENT)];
+  if (randomInteger(COMMENTS_MIN_COUNT, COMMENTS_MAX_COUNT) === COMMENTS_MAX_COUNT) {
+    var firstComment = COMMENTS[randomInteger(0, INDEX_COMMENT_MAX)];
+    var secondComment = COMMENTS[randomInteger(0, INDEX_COMMENT_MAX)];
     while (firstComment === secondComment) {
-      secondComment = COMMENTS[randomInteger(0, MAX_INDEX_COMMENT)];
+      secondComment = COMMENTS[randomInteger(0, INDEX_COMMENT_MAX)];
     }
     photo.comments = [firstComment, secondComment];
   } else {
-    photo.comments = [COMMENTS[randomInteger(0, MAX_INDEX_COMMENT)]];
+    photo.comments = [COMMENTS[randomInteger(0, INDEX_COMMENT_MAX)]];
   }
 };
 
 var createArrayOfPhotos = function () {
   var photos = [];
-  for (var i = 1; i <= PHOTOS; i++) {
+  for (var i = 1; i <= PHOTOS_COUNT; i++) {
     var photo = {};
     photo.url = 'photos/' + i + '.jpg';
-    photo.likes = randomInteger(MIN_LIKES, MAX_LIKES);
+    photo.likes = randomInteger(LIKES_MIN, LIKES_MAX);
     fillPhotoComments(photo);
-    photo.description = DESCRIPTIONS[randomInteger(0, MAX_INDEX_DESCRIPTION)];
+    photo.description = DESCRIPTIONS[randomInteger(0, INDEX_DESCRIPTION_MAX)];
     photos.push(photo);
   }
   return photos;
@@ -84,7 +84,7 @@ var showDetailedPhoto = function (detailedPhoto, photo) {
   detailedPhotoDescription.textContent = photo.description;
 
   for (var i = 0; i < photo.comments.length; i++) {
-    detailedPhotoPicture.src = 'img/avatar-' + (randomInteger(MIN_INDEX_AVATAR, MAX_INDEX_AVATAR)) + '.svg';
+    detailedPhotoPicture.src = 'img/avatar-' + (randomInteger(INDEX_AVATAR_MIN, INDEX_AVATAR_MAX)) + '.svg';
     detailedPhotoText.textContent = photo.comments[i];
     fragment.appendChild(detailedPhotoComment);
   }
@@ -101,7 +101,7 @@ var createPhotoList = function () {
   var similarPhotoList = document.querySelector('.pictures');
   var fragment = document.createDocumentFragment();
 
-  for (var i = 0; i < PHOTOS; i++) {
+  for (var i = 0; i < PHOTOS_COUNT; i++) {
 
     fragment.appendChild(createPhoto(photos[i]));
   }
