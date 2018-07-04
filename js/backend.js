86cf7f213c714a697f4e0b2f15c518fbed4e28f7
Elvina Ibragimova <e.ibragimova@galament.com>
2018-07-04 22:46:33 +0400

    fix backend

--- js/backend.js
+++ js/backend.js
@@ -4,9 +4,6 @@
   var URL_DATA = 'https://js.dump.academy/kekstagram/data';
   var URL_LOAD = 'https://js.dump.academy/kekstagram';
   var SUCCESS_CODE = 200;
-  var AUTORIZATION_ERROR_CODE = 401;
-  var INVALID_QUERY_CODE = 400;
-  var NOTHING_FOUND_CODE = 404;
 
   window.backend = {
     load: function (onLoad, onError) {
@@ -14,22 +11,10 @@
       xhr.responseType = 'json';
 
       xhr.addEventListener('load', function () {
-        switch (xhr.status) {
-          case SUCCESS_CODE:
-            onLoad(xhr.response);
-            break;
-          case INVALID_QUERY_CODE:
-            onError('Неверный запрос');
-            break;
-          case AUTORIZATION_ERROR_CODE:
-            onError('Пользователь не авторизован');
-            break;
-          case NOTHING_FOUND_CODE:
-            onError('Ничего не найдено');
-            break;
-
-          default:
-            onError('Cтатус ответа: : ' + xhr.status + ' ' + xhr.statusText);
+        if (xhr.status === SUCCESS_CODE) {
+          onLoad(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
         }
       });
       xhr.addEventListener('error', function () {
@@ -49,21 +34,10 @@
       xhr.responseType = 'json';
 
       xhr.addEventListener('load', function () {
-        switch (xhr.status) {
-          case SUCCESS_CODE:
-            onLoad(xhr.response);
-            break;
-          case INVALID_QUERY_CODE:
-            onError('Неверный запрос');
-            break;
-          case AUTORIZATION_ERROR_CODE:
-            onError('Пользователь не авторизован');
-            break;
-          case NOTHING_FOUND_CODE:
-            onError('Ничего не найдено');
-            break;
-          default:
-            onError('Cтатус ответа: : ' + xhr.status + ' ' + xhr.statusText);
+        if (xhr.status === SUCCESS_CODE) {
+          onLoad(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
         }
       });
       xhr.addEventListener('error', function () {
